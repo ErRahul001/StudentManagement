30367d7b82023f0df9743ee1d4a9a6f0dd7fb799
ErRahul001 <91515298+ErRahul001@users.noreply.github.com>
2024-02-26 12:06:34 +0530

    Add files via upload

---
+++ src/main/java/com/xadmin/studentmanagement/bean/Student.java
@@ -0,0 +1,36 @@
+package com.xadmin.studentmanagement.bean;
+
+public class Student {
+	private int roll_number;
+	private String name;
+	private String branch;
+	private String college;
+	
+	public tudent
+	public int getRoll_number() {
+		return roll_number;
+	}
+	public void setRoll_number(int roll_number) {
+		this.roll_number = roll_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	
+	
+}
---
+++ src/main/java/com/xadmin/studentmanagement/dao/StudentDao.java
@@ -0,0 +1,5 @@
+package com.xadmin.studentmanagement.dao;
+
+public class StudentDao {
+
+}
